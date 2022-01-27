636d297a119268e70ad0acf939b4581f69333355
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-01-27 14:09:48 +0530

    Add files via upload

---
+++ A-Ditance.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int x,y;cin>>x>>y;
+        if(x>=y)
+        {
+            if((x-y)%2==0)
+            {
+                cout<<"0"<<endl;
+            }
+            else
+            {
+            cout<<"1"<<endl;
+            }
+        }
+        if(x<y)
+        cout<<abs(x-y)<<endl;
+
+    }
+}
